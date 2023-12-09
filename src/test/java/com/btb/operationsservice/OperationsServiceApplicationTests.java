3e84547fa6c056ef831c7790fe42062c4323f163
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-12-09 18:58:43 +0100

    Spring initializr

---
+++ src/test/java/com/btb/operationsservice/OperationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.operationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
